1b8bb7e17874a0c551b42f2cb1aeb43430d739fc
travisyeik <tyeik@huskers.unl.edu>
2023-11-20 10:12:09 -0700

    update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,39 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        _isVisible = false;
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private bool _isVisible;
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+        _isVisible = !_isVisible;
+        BottomGrid.InputTransparent = !_isVisible;
+        BottomGrid.IsVisible = _isVisible;
+    }
+
+    private void Button_OnClicked2(object sender, EventArgs e)
+    {
+        _isVisible = !_isVisible;
+        BottomGrid.InputTransparent = !_isVisible;
+        BottomGrid.IsVisible = _isVisible;
+    }
 }
-
-
